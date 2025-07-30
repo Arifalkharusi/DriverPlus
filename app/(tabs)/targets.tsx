0f9a0895ecc1aeb182088e75ec05642b85cfcd68
Arif Al Kharusi <111698709+Arifalkharusi@users.noreply.github.com>
2025-07-30 13:05:15 +0100

    Updated targets.tsx

--- app/(tabs)/targets.tsx
+++ app/(tabs)/targets.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import { useAuth } from './useAuth';
+import { useAuth } from '@/hooks/useAuth';
 import { createCollection } from '@/utils/mongoHelpers';
 
 interface Expense {
